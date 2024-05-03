38e6ef228054d534eee2e21cc7b78c3229cfea58
Viktor Dyukov <snailrake@mail.ru>
2024-05-03 21:48:37 +0400

    feat: add kafka consumer config

---
+++ src/main/java/school/faang/HashtagServiceApplication.java
@@ -0,0 +1,25 @@
+package school.faang;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.Banner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HashtagServiceApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HashtagServiceApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+}
